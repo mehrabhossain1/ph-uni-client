addec313c04e8cb82a2b48066883950a6203fb38
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-02-09 19:22:07 +0600

    14.3

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, MenuProps } from 'antd';
 import {
   UploadOutlined,
   UserOutlined,
@@ -7,20 +7,33 @@ import {
 import { createElement } from 'react';
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  UserOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: createElement(icon),
-  label: `nav ${index + 1}`,
-}));
-
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    label: 'Dashboard',
+  },
+  {
+    key: '2',
+    label: 'Profile',
+  },
+  {
+    key: '3',
+    label: 'User Management',
+    children: [
+      {
+        key: '11',
+        label: 'Create Admin',
+      },
+      {
+        key: '21',
+        label: 'Create Student',
+      },
+    ],
+  },
+];
 const MainLayout = () => {
   return (
-    <Layout>
+    <Layout style={{ height: '100vh' }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
@@ -31,7 +44,18 @@ const MainLayout = () => {
           console.log(collapsed, type);
         }}
       >
-        <div className="demo-logo-vertical" />
+        <div
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            height: '4rem',
+            display: 'flex',
+            justifyContent: 'center',
+            justifyItems: 'center',
+          }}
+        >
+          <h1>PH Uni</h1>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
