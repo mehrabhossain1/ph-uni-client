34053d3208922208fb2180ce0449b9c5b9bed345
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-02-09 18:52:50 +0600

    14.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import MainLayout from './components/layout/MainLayout';
+
 function App() {
-  return (
-    <>
-      <h1>PH University Management App</h1>
-    </>
-  );
+  return <MainLayout />;
 }
 
 export default App;
