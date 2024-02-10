bb41f71e0c83c2cc5157b92ccddc66c7c18c461b
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-02-10 21:24:49 +0600

    15.1 routesGenerator

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -16,7 +16,7 @@ type TSidebarItem = {
   children?: TSidebarItem[];
 };
 
-const adminPaths = [
+export const adminPaths = [
   {
     name: 'Dashboard',
     path: 'dashboard',
@@ -69,25 +69,25 @@ export const adminSidebarItems = adminPaths.reduce(
   []
 );
 
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//   if (item.path && item.element) {
+//     acc.push({
+//       path: item.path,
+//       element: item.element,
+//     });
+//   }
 
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
+//   if (item.children) {
+//     item.children.forEach((child) => {
+//       acc.push({
+//         path: child.path,
+//         element: child.element,
+//       });
+//     });
+//   }
 
-  return acc;
-}, []);
+//   return acc;
+// }, []);
 
 // export const adminPaths = [
 //   {
