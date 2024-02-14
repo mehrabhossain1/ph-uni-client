c47445fbeb26375886d75760dcb3a8c8a9097b26
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-02-14 21:17:33 +0600

    16.3 authorization token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,43 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
 
+// const baseQuery = fetchBaseQuery({
+//   baseUrl: 'http://localhost:5000/api/v1',
+//   credentials: 'include',
+//   prepareHeaders: (headers, { getState }) => {
+//     const token = (getState() as RootState).auth.token;
+
+//     if (token) {
+//       headers.set('authorization', `${token}`);
+//     }
+
+//     return headers;
+//   },
+// });
+
+// const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+//   const result = await baseQuery(args, api, extraOptions);
+
+//   if (result.error?.status === 401) {
+//     console.log('Sending refresh token');
+
+//     const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+//       method: 'POST',
+//       credentials: 'include',
+//     });
+
+//     const data = await res.json();
+
+//     console.log(data);
+//   }
+// };
+
+// export const baseApi = createApi({
+//   reducerPath: 'baseApi',
+//   baseQuery: baseQueryWithRefreshToken,
+//   endpoints: () => ({}),
+// });
+
 const baseQuery = fetchBaseQuery({
   baseUrl: 'http://localhost:5000/api/v1',
   credentials: 'include',
@@ -15,25 +52,9 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-
-  if (result.error?.status === 401) {
-    console.log('Sending refresh token');
-
-    const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
-      method: 'POST',
-      credentials: 'include',
-    });
-
-    const data = await res.json();
-
-    console.log(data);
-  }
-};
-
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: baseQueryWithRefreshToken,
+  baseQuery: baseQuery,
+
   endpoints: () => ({}),
 });
