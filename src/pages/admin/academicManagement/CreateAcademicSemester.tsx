6f142462ee4d6f7c8aa771692d113c025e893db2
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-02-26 19:50:36 +0600

    18.3 reusable select component

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,18 +1,45 @@
+import { Button, Col, Flex } from 'antd';
 import { FieldValues, SubmitHandler } from 'react-hook-form';
-import PHInput from '../../../components/form/PHInput';
 import PHForm from '../../../components/form/PHForm';
-import { Button } from 'antd';
+import PHSelect from '../../../components/form/PHSelect';
+
+const nameOptions = [
+  {
+    value: '01',
+    label: 'Autumn',
+  },
+  {
+    value: '02',
+    label: 'Summer',
+  },
+  {
+    value: '03',
+    label: 'Fall',
+  },
+];
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    const semesterData = {
+      name: 'something',
+      code: 'something',
+    };
+
+    console.log(semesterData);
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Flex align="center" justify="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          {/* <PHInput type="text" name="name" label="Name" />
+          <PHInput type="text" name="year" label="Year" /> */}
+
+          <PHSelect label="Name" name="name" options={nameOptions} />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
